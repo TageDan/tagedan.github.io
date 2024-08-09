ef2259af572e5a5403e5c743d4202301d8d9ceec
abbTagDan <tage.danielsson@hitachigymnasiet.se>
2024-08-09 18:59:14 +0200

    finished beta version 0.1

--- b.sh
+++ b.sh
@@ -4,16 +4,19 @@
 while [ "$1" = "t" ]
 do
 
-# Compile tailwind
-npx tailwindcss -i ./public/css/input.css -o ./public/css/output.css
 
 # Compile and run rust
 cargo run
+
+
+# Compile tailwind
+npx tailwindcss -i ./public/css/input.css -o ./public/css/output.css
 
 done
 
+# Compile and run rust
+cargo run
+
 # Compile tailwind
 npx tailwindcss -i ./public/css/input.css -o ./public/css/output.css
 
-# Compile and run rust
-cargo run
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ use handlebars::{to_json, Handlebars};
 use serde_json::{json, Value as Json};
 
 use std::{
-    collections::HashMap,
     ffi::OsStr,
-    fs::{self, read_dir, read_to_string, write},
+    fs::{self, read_to_string},
     path::{Path, PathBuf},
 };
 
@@ -78,89 +77,153 @@ impl MarkdownSiteGenerator {
         }
     }
 
-    fn generate(&mut self) {
-        let handlebars = Handlebars::new();
-        let mut templates: HashMap<String, String> = HashMap::new();
-        self.get_templates(&mut templates);
-        let md_files = read_dir(self.content.clone());
-        for md_file in md_files.unwrap() {
-            let md_file = md_file.unwrap();
-            if md_file.path().is_dir() {
-                continue;
-            }
+    fn add_file_path(self, file_name: &str, render_tree: impl Render) -> Self {
+        let html = render_tree.render(&self.templates);
+        let file = file_name.to_owned() + ".html";
+        let path = self.output.clone().join(file);
+        fs::write(path, html).unwrap();
+        self
+    }
 
-            // Getting file_name for post
-
-            let file_name = md_file.path();
-            let file_name = file_name.file_stem().unwrap();
-            // Parsing markdown using pulldown_cmark
-            let (html, md) = parse_markdown(md_file);
-
-            // Getting metadata for post
-
-            let Metadata {
-                title,
-                author,
-                description,
-                tags,
-                date,
-            } = get_metadata::<Metadata>(md);
-
-            // Generating new content using handlebars
-            let content = handlebars
-                .render_template(
-                    &templates.get("post").unwrap(),
-                    &json!({"title": title, "author": author,  "content": html }),
-                )
-                .unwrap();
-
-            let content = handlebars
-                .render_template(
-                    &templates.get("base").unwrap(),
-                    &json!({"title": title, "content": content}),
-                )
-                .unwrap();
-
-            // Writing content to static html_file
-            let _ = write(
-                self.output
-                    .join(self.content.clone())
-                    .join(&format!("{}.html", file_name.to_str().unwrap())),
-                content,
-            )
-            .unwrap();
+    fn add_content_folder_path<T: Serialize + DeserializeOwned>(
+        self,
+        folder_name: &str,
+        content_render_tree: impl Render,
+    ) -> Self {
+        let output_folder = self.output.clone().join(folder_name);
+        fs::create_dir_all(&output_folder).unwrap();
+        for file in fs::read_dir(self.content.clone().join(folder_name)).unwrap() {
+            let f = file.unwrap();
+            let file_path = f.path();
+            let file_stem = file_path.file_stem().unwrap();
+            let file_name = file_stem.to_str().unwrap();
+            let file_name = file_name;
+            let html = content_render_tree.render_for_file::<T>(
+                &self.templates,
+                &self.content,
+                &(folder_name.to_owned() + file_name),
+            );
+            let file = file_name.to_owned() + ".html";
+            let path = output_folder.join(file);
+            fs::write(path, html).unwrap();
         }
-    }
-
-    fn get_templates(&self, templates: &mut HashMap<String, String>) {
-        let template_files = read_dir(self.templates.clone()).unwrap();
-        for file in template_files {
-            let file = file.unwrap();
-            if file.path().is_dir() {
-                continue;
-            }
-            let file_name = String::from(file.path().file_stem().unwrap().to_str().unwrap());
-            let html_string = read_to_string(file.path()).unwrap();
-            templates.insert(file_name, html_string);
-        }
-    }
-
-    fn add_file_path(&self, arg: &str, content_metadata_list: impl Render) -> Self {
-        todo!()
-    }
-
-    fn add_markdown_path(&self) -> Self {
-        todo!()
+        self
     }
 }
 
-trait Render {}
+trait Render {
+    fn handlebars() -> Handlebars<'static> {
+        Handlebars::new()
+    }
+    fn render(&self, template_folder: &Path) -> String;
+    fn render_for_file<T>(
+        &self,
+        template_folder: &Path,
+        content_folder: &Path,
+        file_name: &str,
+    ) -> String
+    where
+        T: Serialize,
+        T: DeserializeOwned;
+}
 
-impl<C> Render for (&str, C) where C: Render {}
+impl<C> Render for (&str, C)
+where
+    C: Render,
+{
+    fn render(&self, template_folder: &Path) -> String {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        Self::handlebars()
+            .render_template(
+                &template,
+                &json!({"content": self.1.render(template_folder)}),
+            )
+            .unwrap()
+    }
+    fn render_for_file<T>(
+        &self,
+        template_folder: &Path,
+        content_folder: &Path,
+        file_name: &str,
+    ) -> String
+    where
+        T: Serialize,
+        T: DeserializeOwned,
+    {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        Self::handlebars()
+            .render_template(
+                &template,
+                &json!({"content": self.1.render_for_file::<T>(template_folder, content_folder, file_name)}),
+            )
+            .unwrap()
+    }
+}
 
-impl Render for &str {}
+impl Render for &str {
+    fn render(&self, template_folder: &Path) -> String {
+        let file = self.to_owned().to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        Self::handlebars()
+            .render_template(&template, &json!({}))
+            .unwrap()
+    }
+    fn render_for_file<T>(
+        &self,
+        template_folder: &Path,
+        content_folder: &Path,
+        file_name: &str,
+    ) -> String
+    where
+        T: Serialize + DeserializeOwned,
+    {
+        let file = self.to_owned().to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        let file = file_name.to_owned() + ".md";
+        let (html, md) = parse_markdown(&content_folder.join(file));
+        let mut json = to_json(get_metadata::<T>(md));
+        merge(&mut json, &json!({"content": html}));
+        Self::handlebars()
+            .render_template(&template, &json)
+            .unwrap()
+    }
+}
 
-impl Render for (&str, Json) {}
+impl Render for (&str, Json) {
+    fn render_for_file<T>(
+        &self,
+        template_folder: &Path,
+        content_folder: &Path,
+        file_name: &str,
+    ) -> String
+    where
+        T: Serialize,
+        T: DeserializeOwned,
+    {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        let file = file_name.to_owned() + ".md";
+        let (html, md) = parse_markdown(&content_folder.join(file));
+        let mut json = to_json(get_metadata::<T>(md));
+        merge(&mut json, &self.1);
+        merge(&mut json, &json!({"content": html}));
+        Self::handlebars()
+            .render_template(&template, &json)
+            .unwrap()
+    }
+    fn render(&self, template_folder: &Path) -> String {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(&file)).expect(&format!(
+            "Can't find file {:?}",
+            template_folder.join(&file)
+        ));
+        Self::handlebars()
+            .render_template(&template, &self.1)
+            .unwrap()
+    }
+}
 
 fn merge(a: &mut Json, b: &Json) {
     match (a, b) {
@@ -175,10 +238,47 @@ fn merge(a: &mut Json, b: &Json) {
     }
 }
 
-impl<C> Render for (&str, Json, C) where C: Render {}
+impl<C> Render for (&str, Json, C)
+where
+    C: Render,
+{
+    fn render(&self, template_folder: &Path) -> String {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        let mut json = self.1.clone();
+        merge(
+            &mut json,
+            &json!({"content": self.2.render(template_folder)}),
+        );
+        Self::handlebars()
+            .render_template(&template, &json)
+            .unwrap()
+    }
+    fn render_for_file<T>(
+        &self,
+        template_folder: &Path,
+        content_folder: &Path,
+        file_name: &str,
+    ) -> String
+    where
+        T: Serialize,
+        T: DeserializeOwned,
+    {
+        let file = self.0.to_owned() + ".html";
+        let template = fs::read_to_string(template_folder.join(file)).unwrap();
+        let mut json = self.1.clone();
+        merge(
+            &mut json,
+            &json!({"content": self.2.render_for_file::<T>(template_folder, content_folder, file_name)}),
+        );
+        Self::handlebars()
+            .render_template(&template, &json)
+            .unwrap()
+    }
+}
 
-fn parse_markdown(md_file: fs::DirEntry) -> (String, String) {
-    let md = read_to_string(md_file.path()).unwrap();
+fn parse_markdown(md_file: &Path) -> (String, String) {
+    let md = read_to_string(md_file).expect(&format!("Can't find file {:?}", md_file));
     let meta_data_options = Options::ENABLE_YAML_STYLE_METADATA_BLOCKS;
     let parser = Parser::new_ext(&md, meta_data_options);
     let mut html = String::new();
@@ -194,12 +294,19 @@ where
     result.metadata
 }
 
-fn get_all_metadata<T>() -> Vec<T> {
-    todo!()
+fn get_all_metadata<T: DeserializeOwned>(md_folder: &'static str) -> Vec<T> {
+    let mut res = Vec::new();
+    for file in fs::read_dir(md_folder).unwrap() {
+        let file = file.unwrap().path();
+        let content = fs::read_to_string(file).unwrap();
+        let meta = get_metadata::<T>(content);
+        res.push(meta)
+    }
+    res
 }
 
 fn main() {
-    let mut generator = MarkdownSiteGenerator::default()
+    MarkdownSiteGenerator::default()
         .add_file_path(
             "index",
             (
@@ -207,9 +314,12 @@ fn main() {
                 json!({"title": "Blog"}),
                 (
                     "index",
-                    ("post_list", json!({"posts":get_all_metadata::<Metadata>()})),
+                    (
+                        "post_list",
+                        json!({"posts": get_all_metadata::<Metadata>("./content/posts")}),
+                    ),
                 ),
             ),
         )
-        .add_markdown_path();
+        .add_content_folder_path::<Metadata>("posts/", ("base", "post"));
 }
